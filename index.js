dfc0c8f7dd2ce56767279cb6210f53d0b37ea069
Jesson Yo <Aphostrophy@gmail.com>
2021-07-08 22:24:06 +0700

    milestone

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const { fork } = require('child_process')
+const { fork } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const lockFile = require('lockfile');
 
 const { google } = require('googleapis');
 const keys = require('./keys.json');
 
-const ipc = require('')
+const filePath = path.resolve(__dirname, 'shared.json');
+const lockPath = path.resolve(__dirname, 'shared.json.lock');
 
+/* Child Processes */
 const childProcess0 = fork('./typer.js');
 const childProcess1 = fork('./typer.js');
 const childProcess2 = fork('./typer.js');
@@ -14,24 +19,47 @@ const childProcess3 = fork('./typer.js');
 const pool = [{ active: false, sent: false }, { active: false, sent: false }, { active: false, sent: false }, { active: false, sent: false }];
 
 childProcess0.on("message", (message) => {
-  console.log(message.status, "Message received successfully");
-  console.log(message.data);
+  console.log(message.status, "Message received successfully from bot 0");
+  setTimeout(() => {
+    sendProcess0();
+  }, 1000 * (Math.floor(Math.random() * 6) + 1));
 });
 
-const sendProcess0 = (dataArray,exit) => {
-  childProcess0.send({ "dataArray": dataArray, "exit": exit });
+childProcess1.on("message", (message) => {
+  console.log(message.status, "Message received successfully from bot 1");
+  setTimeout(() => {
+    sendProcess1();
+  }, 1000 * (Math.floor(Math.random() * 6) + 1));
+});
+
+childProcess2.on("message", (message) => {
+  console.log(message.status, "Message received successfully from bot 2");
+  setTimeout(() => {
+    sendProcess2();
+  }, 1000 * (Math.floor(Math.random() * 6) + 1));
+});
+
+childProcess3.on("message", (message) => {
+  console.log(message.status, "Message received successfully from bot 3");
+  setTimeout(() => {
+    sendProcess3();
+  }, 1000 * (Math.floor(Math.random() * 6) + 1));
+});
+
+const sendProcess0 = () => {
+  childProcess0.send("Bot 0");
 }
 
-const sendProcess1 = (dataArray,exit) => {
-  childProcess1.send({ "dataArray": dataArray, "exit": exit });
+const sendProcess1 = () => {
+  childProcess1.send("Bot 1");
 }
 
-const sendProcess2 = (dataArray,exit) => {
-  childProcess2.send({ "dataArray": dataArray, "exit": exit });
+const sendProcess2 = () => {
+  childProcess2.send("Bot 2");
 }
 
-const sendProcess3 = (dataArray,exit) => {
-  childProcess3.send({ "dataArray": dataArray, "exit": exit });
+const sendProcess3 = () => {
+  childProcess3.send("Bot 3");
 }
 
 const main = async () => {
@@ -48,22 +76,36 @@ const main = async () => {
 
       const opt = {
         spreadsheetId: '1VBbZop6BgUrR-zXVrYMttsgcKQZg6-95HQbUMleL7ac',
-        range: 'A2:B5'
+        range: 'A2:B7'
       }
       let { data: res } = await gsapi.spreadsheets.values.get(opt);
       let dataArray = res.values;
+      let gsData = dataArray.map((data) => ({
+        id: data[0],
+        name: data[1],
+      }))
+      
+      fs.writeFileSync(filePath, JSON.stringify({
+        botFile: {
+          bot0: false,
+          bot1: false,
+          bot2: false,
+          bot3: false,
+        },
+        data: gsData
+      }), 'utf8');
 
       gsrun(client, dataArray);
     }
   });
 }
 
-const gsrun = async(client,dataArray)=>{
+const gsrun = async()=>{
   try {
-    sendProcess0(dataArray, false);
-    sendProcess0(dataArray, false);
-    sendProcess0(dataArray, false);
-    sendProcess0(dataArray, true);
+    sendProcess0();
+    sendProcess1();
+    sendProcess2();
+    sendProcess3();
   } catch (err) {
     console.log(err);
   }
--- typer.js
+++ typer.js
@@ -1,17 +1,27 @@
 const { google } = require('googleapis');
 const keys = require('./keys.json');
+const lockFile = require('lockfile');
+const fs = require('fs');
+const path = require('path');
+
+// Fungsi utilitas untuk memaksa bot menunggu
+const delay = ms => new Promise(res => setTimeout(res, ms)); 
+
+const filePath = path.resolve(__dirname, 'shared.json');
+const lockPath = path.resolve(__dirname, 'shared.json.lock');
+
+const lockOptions = {
+  wait: 1000,
+  stale: 5000,
+  retries: 100,
+  retryWait: 1000
+};
 
 process.on("message", (message) => {
-  if (message.exit) {
-    console.log("Exit");
-    writeGSMessage(message.dataArray,true)
-  } else {
-    console.log("No exit");
-    writeGSMessage(message.dataArray,false);
-  }
+  writeGSMessage(message);
 })
 
-const writeGSMessage = async (dataArray,close) => {
+const writeGSMessage = async (message) => {
   const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
 
   client.authorize(async (err, token) => {
@@ -20,21 +30,88 @@ const writeGSMessage = async (dataArray,close) => {
       return;
     } else {
       const gsapi = google.sheets({ version: 'v4', auth: client });
-      dataArray.push([parseInt(dataArray[dataArray.length - 1][0]) + 1, 'Marcello']);
 
-      const updateOpt = {
-        spreadsheetId: '1VBbZop6BgUrR-zXVrYMttsgcKQZg6-95HQbUMleL7ac',
-        range: 'A2',
-        valueInputOption: 'USER_ENTERED',
-        resource: { values: dataArray }
-      }
-      res = await gsapi.spreadsheets.values.update(updateOpt);
+      let botList = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      botList = botList.botFile;
 
-      process.send({ "status": "ok", "data": dataArray });
-      if (close) {
-        console.log("closing");
-        process.exit();
+      if (message === "Bot 0") {
+        while (botList.bot1 !== true || botList.bot3 !== true) {
+          delay(1000);
+          if (!fs.existsSync(lockPath)) {
+            try {
+              botList = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+              botList = botList.botFile;
+            } catch (err) {
+              console.error(err);
+            }
+          }
+        }
       }
+
+      while (fs.existsSync(lockPath)) {
+        delay(1000);
+      }
+
+      lockFile.lock(lockPath, lockOptions, async (error) => {
+
+        if (error) { console.error(error); }
+          console.log(message + " is trying to write");
+
+        // // Code ini seharusnya ke run kalo bot sudah pasti menulis
+        let oldJson = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    
+        // Save new data to the existing JSON file
+        fs.writeFileSync(filePath, JSON.stringify(oldJson), 'utf8');
+
+        oldJson = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        let dataArray = oldJson.data.map((data) => {
+          return [data.id, data.name];
+        })
+        let botFile = oldJson.botFile;
+  
+        dataArray.push([parseInt(dataArray[dataArray.length - 1][0]) + 1, Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),message]);
+  
+        const updateOpt = {
+          spreadsheetId: '1VBbZop6BgUrR-zXVrYMttsgcKQZg6-95HQbUMleL7ac',
+          range: 'A2',
+          valueInputOption: 'USER_ENTERED',
+          resource: { values: dataArray }
+        }
+        res = await gsapi.spreadsheets.values.update(updateOpt);
+
+        let gsData = dataArray.map((data) => ({
+          id: data[0],
+          name: data[1],
+        }))
+
+        oldJson.data = gsData;
+
+        oldJson.botFile = botFile;
+
+        if (message === "Bot 0") {
+          botFile.bot1 = false;
+          botFile.bot2 = false;
+          botFile.bot3 = false;
+        } else if (message === "Bot 1") {
+          botFile.bot1 = true;
+        } else if (message === "Bot 2") {
+          botFile.bot1 = false;
+          botFile.bot3 = false;
+        } else if (message === "Bot 3") {
+          botFile.bot3 = true;
+        }
+
+        fs.writeFileSync(filePath, JSON.stringify(oldJson), 'utf8');
+    
+        lockFile.unlock(lockPath, (error) => {
+    
+          if (error) { console.error(error); }
+          // console.log(message + " finished with shared file");
+          else {
+            process.send({ "status": "ok", "data": dataArray });
+          }
+        });
+      });
     }
   })
 }
